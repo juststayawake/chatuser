2c42df1fccfd2adabe2fc9423656cc6fab579e00
ninvfeng <ninvfeng@qq.com>
2023-03-28 23:28:57 +0800

    Update Charge.tsx

--- src/components/Charge.tsx
+++ src/components/Charge.tsx
@@ -102,14 +102,14 @@ export default (props: Props) => {
                         请选择充值金额
                     </span>
                     <div class="flex space-x-2">
-                        <button onClick={()=>{getPaycode(1)}} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-                            1元60次
+                        <button onClick={()=>{getPaycode(18.8)}} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
+                            18.8元100次
                         </button>
-                        <button onClick={()=>{getPaycode(5)}} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-                            5元320次
+                        <button onClick={()=>{getPaycode(29.8)}} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
+                            29.8元200次
                         </button>
-                        <button onClick={()=>{getPaycode(10)}} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-                            10元650次
+                        <button onClick={()=>{getPaycode(68.8)}} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
+                            68.8元500次
                         </button>
                     </div>
                 </Show>
